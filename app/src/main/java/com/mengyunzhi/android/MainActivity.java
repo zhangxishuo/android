ccb54d15ecc204671986429180a8ade00837a9d1
zhangxishuo <zhangxishuo1998@gmail.com>
2019-02-09 19:31:25 +0800

    architecture-design

--- app/src/main/java/com/mengyunzhi/android/MainActivity.java
+++ app/src/main/java/com/mengyunzhi/android/MainActivity.java
@@ -1,18 +1,12 @@
 package com.mengyunzhi.android;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Toast;
+import com.mengyunzhi.core.activity.YunzhiActivity;
+import com.mengyunzhi.core.fragment.YunzhiFragment;
 
-import com.mengyunzhi.core.Yunzhi;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends YunzhiActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toast.makeText(Yunzhi.getContext(), "Context is success.", Toast.LENGTH_LONG).show();
+    public YunzhiFragment setRootFragment() {
+        return new MyFragment();
     }
 }
---
+++ app/src/main/java/com/mengyunzhi/android/MyFragment.java
@@ -0,0 +1,11 @@
+package com.mengyunzhi.android;
+
+import com.mengyunzhi.core.fragment.YunzhiFragment;
+
+public class MyFragment extends YunzhiFragment {
+
+    @Override
+    public Object setLayout() {
+        return R.layout.fragment_my ;
+    }
+}
